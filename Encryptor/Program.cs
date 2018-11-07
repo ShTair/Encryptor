08a7ac97a2938b2ddb73da7433bdc532f69506a5
ShTair <git@vsh.jp>
2018-11-07 18:17:22 +0900

    ZIP作成を実装

--- Encryptor/Program.cs
+++ Encryptor/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ICSharpCode.SharpZipLib.Zip;
+using System;
+using System.IO;
+using System.Linq;
 
 namespace Encryptor
 {
@@ -6,7 +9,32 @@ namespace Encryptor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var files = args.Where(t => File.Exists(t)).ToList();
+            if (files.Count == 0)
+            {
+                Console.WriteLine("ファイルを、exeにドラッグドロップしてください。");
+                Console.ReadLine();
+                return;
+            }
+
+            var name = Path.ChangeExtension(files[0], ".zip");
+            if (File.Exists(name))
+            {
+                Console.WriteLine($"\"{name}\"");
+                Console.WriteLine("を作成しようと思いましたが、既に存在します。");
+                Console.ReadLine();
+                return;
+            }
+
+            var zip = ZipFile.Create(name);
+            zip.BeginUpdate();
+
+            foreach (var file in files)
+            {
+                zip.Add(file);
+            }
+
+            zip.CommitUpdate();
         }
     }
 }
